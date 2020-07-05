3e34c5400ca570daea2d3bf08a1dcafed060216f
maru25-alt <53083980+maru25-alt@users.noreply.github.com>
2020-07-05 14:42:54 +0800

    build

---
+++ build/precache-manifest.9da01a892fb94318cafa0ade2f750a1e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0066bc5a292fc78c71794d60cb9abf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e50f6a7db9f78e8cb8c",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "cdef7a8169c5b7e4714e",
+    "url": "/static/css/main.4ef3879c.chunk.css"
+  },
+  {
+    "revision": "6e50f6a7db9f78e8cb8c",
+    "url": "/static/js/2.819d9c4b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.819d9c4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdef7a8169c5b7e4714e",
+    "url": "/static/js/main.a57a3776.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  },
+  {
+    "revision": "82e8f2e3e7136f621529092314e2dd26",
+    "url": "/static/media/logo.82e8f2e3.jpg"
+  },
+  {
+    "revision": "87c55bcec393f3019008c7dabc5b91c8",
+    "url": "/static/media/photo-profile.87c55bce.jpeg"
+  }
+]);
